e34bd6b828cecb13438b07fd682b3f84f126acd7
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-10-25 10:57:36 -0700

    add input and button to get note value

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 
 const HomePage = () => {
+   const [note, setNote] = useState('');
+
+   const getNoteValue = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+      setNote(e.target.value);
+   }
+
+   const onAddNoteClick = () => {
+      console.log('what is this value? ', note);
+   }
    return (
-      <div><h3>he</h3></div>
+      <div>
+         <input onChange={getNoteValue} value={note} type="text" name="note" placeholder="enter note"/>
+         <button onClick={onAddNoteClick}>add note</button>
+      </div>
    )
 }
 
