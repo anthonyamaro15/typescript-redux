ee47be17beb073270bac788709bc821a6e3b02fb
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-10-25 14:43:32 -0700

    wrapper input and button in a form

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -11,14 +11,18 @@ const HomePage: React.FC<Props> = ({addNote}) => {
       setNote(e.target.value);
    }
 
-   const onAddNoteClick = () => {
+
+   const onSubmit = (e: { preventDefault: () => void; }) => {
+      e.preventDefault();
       addNote(note);
       setNote('');
    }
    return (
       <div>
-         <input onChange={getNoteValue} value={note} type="text" name="note" placeholder="enter note"/>
-         <button onClick={onAddNoteClick}>add note</button>
+         <form onSubmit={onSubmit}>
+            <input onChange={getNoteValue} value={note} type="text" name="note" placeholder="enter note"/>
+            <button type="submit">add note</button>
+         </form>
       </div>
    )
 }
